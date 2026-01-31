8f028f603ec98e954c49f10109c46e32901aadb7
Kaveri123319 <daddykaavi@gmail.com>
2026-01-31 15:23:48 +0530

    Add files via upload

--- demo.java
+++ demo.java
@@ -1,3 +1,9 @@
-public class demo{
-    
+class demo
+{
+    public static void main(String args[]){
+        int a=10;
+        System.out.println(a);
+        a=20;
+        System.out.println(a);
+    }
 }
